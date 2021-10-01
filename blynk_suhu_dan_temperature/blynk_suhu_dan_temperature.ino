c7616cfd81bab8755f062a53d7669fe25f7f2fd4
Gilbram09 <55072835+Gilbram09@users.noreply.github.com>
2021-10-01 13:54:30 +0700

    Add files via upload

---
+++ blynk_suhu_dan_temperature/blynk_suhu_dan_temperature.ino
@@ -0,0 +1,44 @@
+#define BLYNK_PRINT Serial
+
+#include <DHT.h>
+#include <ESP8266WiFi.h> //file untuk modul wifi
+#include <BlynkSimpleEsp8266.h> //file untuk blynk
+
+char auth[] = "Gw9Fs5tqyO9knwq80J9lxn-UUhVoEu74"; //char yang dikirim oleh blynk
+char ssid[] = "WiFi";
+char pass[] = "apaandahh";
+
+#define DHTPIN 14
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+BlynkTimer timer;
+
+void sendSensor() {
+float h = dht.readHumidity();
+float t = dht.readTemperature();
+
+if (isnan(h) || isnan(t)) { //mengecek kondisi suhu / kelembapan tidak ada
+  Serial.println("Failed to read temp and huminity");
+  return;
+  }
+  Blynk.virtualWrite(V1, t); //setting virtual suhu
+  Blynk.virtualWrite(V2, h); //setting virtual kelembapan
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Blynk.begin(auth, pass, ssid);
+  dht.begin(); //sensor mulai membaca suhu dan kelembapan
+  timer.setInterval(1000, sendSensor); //waktu untuk mengirimkan data
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Blynk.run();
+  timer.run();
+
+}
